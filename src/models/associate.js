03684f970c3298244d2a38c0032a88abe11b17a0
Fabien Winkler <fabien.winkler@outlook.fr>
2020-12-10 00:01:15 +0100

    <Feat> ajout table mode de jeu + association

--- src/models/associate.js
+++ src/models/associate.js
@@ -2,48 +2,50 @@ module.exports = (sequelize) => {
     sequelize.models.Tournament.belongsToMany(sequelize.models.Game, {
         through: 'tournament_game',
         as: 'games',
-        foreignKey: 'tournament_id'
     });
     sequelize.models.Game.belongsTo(sequelize.models.Tournament, {
         through: 'tournament_game',
         as: 'tournaments',
-        foreignKey: 'game_id'
     });
 
 
     sequelize.models.Tournament.belongsToMany(sequelize.models.Platform, {
         through: 'tournament_platform',
         as: 'platforms',
-        foreignKey: 'tournament_id'
     });
     sequelize.models.Platform.belongsTo(sequelize.models.Tournament, {
         through: 'tournament_platform',
         as: 'tournaments',
-        foreignKey: 'platform_id'
     });
 
 
     sequelize.models.Game.belongsToMany(sequelize.models.Platform, {
         through: 'platform_game',
         as: 'platforms',
-        foreignKey: 'game_id'
     });
 
     sequelize.models.Platform.belongsTo(sequelize.models.Game, {
         through: 'platform_game',
         as: 'games',
-        foreignKey: 'platform_id'
     });
 
 
     sequelize.models.Tournament.belongsToMany(sequelize.models.User, {
         through: 'user_tournament',
         as: 'users',
-        foreignKey: 'tournament_id'
     });
     sequelize.models.User.belongsTo(sequelize.models.Tournament, {
         through: 'user_tournament',
         as: 'tournaments',
-        foreignKey: 'user_id'
+    });
+
+    
+    sequelize.models.Game.belongsToMany(sequelize.models.game_mode, {
+        through: 'mode_per_game',
+        as: 'gamesMode',
+    });
+    sequelize.models.game_mode.belongsToMany(sequelize.models.Game, {
+        through: 'mode_per_game',
+        as: 'games',
     });
 }
---
+++ src/models/gameMode.model.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, Sequelize) => {
+    const GameMode = sequelize.define('game_mode', {
+        // Model attributes are defined here
+        name: {
+            type: Sequelize.STRING(255),
+            allowNull: false
+        },
+        createdBy: {
+            type: Sequelize.STRING(255),
+            allowNull: false
+        },
+        createdAt: {
+            type: Sequelize.DATE,
+            defaultValue: sequelize.literal('NOW()'),
+            allowNull: false
+        },
+        updatedBy : {
+            type: Sequelize.STRING(255),
+            allowNull: true
+        },
+        updatedAt: {
+            type: Sequelize.DATE,
+            allowNull: true
+        }
+    }, { freezeTableName: true });
+
+    return GameMode;
+}
